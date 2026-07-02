56500175f3f0c84f0b1fff5b5090eda87d97f4cb
Abiona Israel <israelabiona44@gmail.com>
2026-07-02 16:39:48 +0100

    Update NewsBar and NewsItem components for improved navigation and layout; add active class to category links and adjust NewsItem styling.

--- src/Components/NewsBar.jsx
+++ src/Components/NewsBar.jsx
@@ -4,28 +4,25 @@ const NewsBar = ({setCategory}) => {
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
     <div className="container-fluid">
     <a className="navbar-brand" href="#"><span className="bg-light text-dark w-20 p-2">HotNews <span class="badge text-bg-secondary">4</span></span></a>
-    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button> */}
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("technology")}>Technology</div>
+          <div className="nav-link active" onClick={() => setCategory("technology")}>Technology</div>
         </li>
         <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("health")}>Health</div>
+          <div className="nav-link active" onClick={() => setCategory("health")}>Health</div>
         </li>
         <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("science")}>Science</div>
+          <div className="nav-link active" onClick={() => setCategory("science")}>Science</div>
         </li>
         <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("business")}>Business</div>
+          <div className="nav-link active" onClick={() => setCategory("business")}>Business</div>
         </li>
         <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("sports")}>Sports</div>
+          <div className="nav-link active" onClick={() => setCategory("sports")}>Sports</div>
         </li>
         <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("entertainment")}>Entertainment</div>
+          <div className="nav-link active" onClick={() => setCategory("entertainment")}>Entertainment</div>
         </li>
       </ul>
       <form className="d-flex" role="search">
